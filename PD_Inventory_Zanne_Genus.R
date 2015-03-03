d4571667219818621ef708ba2ee1c05f5d8b3287
Fabian Roger <Fabian_Roger@gmx.de>
2015-03-03 17:52:00 +0100

    added comments and deleted unecessary code (leftovers form parent file Zanne & Walter trees)

--- PD_Inventory_Zanne_Genus.R
+++ PD_Inventory_Zanne_Genus.R
@@ -13,11 +13,11 @@
 # set your working directory (the trees and Rsession will be saved here)
 # setwd()
 
-library("ape")
-library("ggplot2")
 library("picante")
 library("reshape2")
 library("data.table")
+library("vegan")
+library("plyr")
 
 # first time #
 ######################################################################
@@ -36,51 +36,10 @@ library("data.table")
 ######################################################################
 
 
-# load workspace
+# load workspace with Zannes Tree
 load("Tree.RData")
 
-# list of tree species in the experiments (Kreinitz, FORBIO, ORPHEE, Bangor, B-Tree, 
-# Satakunta, BioTree) and all the Exploratory Sites (see below for inventory species included)
-
-TreeSp<-c("Abies_alba","Acer_campestre","Acer_platanoides","Acer_pseudoplatanus",
-          "Alnus_glutinosa","Betula_pendula","Carpinus_betulus","Castanea_sativa",
-          "Fagus_sylvatica","Fraxinus_excelsior","Larix_decidua","Larix_eurolepis",
-          "Larix_kaempferi","Larix_sibirica","Ostrya_carpinifolia","Picea_abies",
-          "Pinus_nigra","Pinus_pinaster","Pinus_sylvestris","Populus_tremula",
-          "Prunus_avium","Pseudotsuga_menziesii","Quercus_cerris","Quercus_faginea",
-          "Quercus_ilex","Quercus_petraea","Quercus_pyrenaica","Quercus_robur",
-          "Sorbus_aucuparia","Sorbus_torminalis","Tilia_cordata","Ulmus_glabra")
-
-
-# check which of the Species are in the Tree
-
-TreeSp[which (! TreeSp %in% Tree$tip.label )] 
-
-# The only missing species is  "Larix_eurolepis" which is a hybrid species between
-# "Larix_decidua" and "Larix_kaempferi" (both of which are in the tree)
-# "Larix_eurolepis" is only present in the FORBIO experiment
-
-#exclude "Larix_eurolepis"
-
-TreeSp<-TreeSp[-c(which (! TreeSp %in% Tree$tip.label ))]
-
-###################################################################################
-### extract subtree with only the species in the Experiments and  Exploratories ### 
-###################################################################################
-
-# Vector of Tips to drop
-Drop <- Tree$tip.label[-c(which(Tree$tip.label %in% TreeSp))]
-
-# substree with all species in Experiments and Exploratories
-TreeExp<-drop.tip(Tree,Drop)
-
-# export Subtree
-write.tree(TreeExp, "TreeExp.tree")
-
-###############################################################
-###  Iventroy Species (taken from FunDivEUROPE_species.csv) ### 
-###############################################################
-
+# read in the inventory Species list from Sophia
 SPInv <- read.table("FunDivTrees.txt",header=T,sep="\t")
 
 # check which species are not in Phylogeny
@@ -97,7 +56,7 @@ SPInv[which (! sub("(\\w)_.+","\\1",SPInv$PD.code) %in%
 # Visnea
 
 #subset Tree to include only genuses present in Inventory dataset
-# create tree with only one species per genus (first species per genus kept)
+#and create tree with only one species per genus (first species per genus kept)
 
 # vector of Genuses in inventory
 INV.G <- unique(sub("(\\w)_.+","\\1",SPInv$PD.code))
@@ -109,6 +68,7 @@ TreeInv<-drop.tip(Tree,Drop)
 # keep only the first species per genus
 Drop2<- TreeInv$tip.label[-c(match(unique(sub("(\\w)_.+","\\1",TreeInv$tip.label))
               , sub("(\\w)_.+","\\1",TreeInv$tip.label)))]
+
 TreeInvG <- drop.tip(TreeInv,Drop2)
 
 # cut tip labels to genus only
@@ -116,7 +76,7 @@ TreeInvG$tip.label <- sub("(\\w)_.+","\\1",TreeInvG$tip.label)
 
 ###############################################################################
 
-########## comparision between Genus based PD and Species PD ##################
+########## calculate PD at Genus level ########################################
 
 # load inventory species matrix ("species.matrix", structure is dataframe)
 # contains all plots in Inventories with unique plotcode (rows) and species /
@@ -125,18 +85,20 @@ TreeInvG$tip.label <- sub("(\\w)_.+","\\1",TreeInvG$tip.label)
 
 load("upscale_inventory_species_matrix_ba_ha.RData")
 
+#######################
+#group species to Genus level (add basal are in each plot of all species of the same Genus) #
+#######################
 
-# group species to Genus level (add basal are in each plot of all species of the same Genus) #
 
 # I work with data.table as "melt" from the reshape2 package and "ddply" from the plyr package take very long on 
 # dataframe
 
-# transform first to matrix as somehow I can't get rid of the cast_df appendicies of species.matrix 
+# transform first to matrix as somehow I can't get rid of the cast_df appendicies of "species.matrix"
 specM <- as.matrix(species.matrix[,-1])
 dimnames(specM)<-list(species.matrix$plotcode,colnames(species.matrix)[-1])
 specDT <- as.data.table(specM)
 
-# adding plotcode as data.table 
+# adding plotcode to data.table 
 specDT[,plotcode := species.matrix$plotcode]
 
 #reshape dataframe in long format
@@ -156,38 +118,66 @@ spec_Genus_w <- dcast.data.table(spec_Genus, plotcode~genus) ######## data.table
 species.matrix.g <- as.matrix(spec_Genus_w[,-1, with=F])
 rownames(species.matrix.g) <- spec_Genus_w[[1]]
 
-######## calculate Phylogentic diversity #########
+##################################################
+# calculate Phylogentic diversity 
+##################################################
 
 # Transform Genus code in TreeInvG to match genus code in species.matrix.g
-
 TreeInvG$tip.label <- toupper(substr(TreeInvG$tip.label,1,3))
 
 # match species in Tree with species present in species.matrix
+#### OBS: all Genus in the species.matrix file are present in the Tree and the file contains fewer Genuses than the list above!
+
 TreeInvGp <- prune.sample(species.matrix.g, TreeInvG)
+
+#calculate psv, psr, pse  [Helmus et al 2007; Phylogenetic measures of biodiversity.]
+#calculate pd; [Faith D.P. (1992) Conservation evaluation and phylogenetic diversity.]
+
+#### psv: phylogenetic species variability ####
+
+# quantifies how phylogenetic relatedness decreases 
+# the variance of a hypothetical unselected/neutral 
+# trait shared by all species in a community. 
+# bound between 0 and 1 (1 = max variability; all sp. unrelated)
+
+#### psr: phylogenetic species eveness ####
+
+# modified psv that incorporates abundance information
+# bound between 0 and 1 (1 = copletely even community with star phylogeny)
+
+#### psr: phylogenetic species richness ####
+
+# psv x richness
+# "scales" the richness (S) of the community by it's psv
+# bound between 0 and S
+# often strongly correlated to S 
 
 psv.result<-psv(species.matrix.g, TreeInvGp)
 psv.result$plotcode <- rownames(psv.result)
 
-pse.result<-pse(species.matrix.g, TreeInvGp)
+pse.result<-pse(species.matrix.g, TreeInvGp) # basal are is taken as species abundance
 pse.result$plotcode <- rownames(pse.result)
 
 psr.result<-psr(species.matrix.g, TreeInvGp)
 psr.result$plotcode <- rownames(psr.result)
 
 # pd seems to fail if sites with richness 1 are included wherfore I exclude them manually
-
-## OBS pd take quite long to run! ###
+## OBS pd takes ~2 minutes  to run! ###
 pd.result<-pd(species.matrix.g[which(specnumber(species.matrix.g) > 1),], TreeInvGp)
 pd.result$plotcode <- rownames(pd.result)
 
-
+# join the different metrics to common dataframe
 PhylDiv<-join(psv.result,pse.result,by="plotcode")
 PhylDiv<-join(PhylDiv,psr.result,by="plotcode")
 PhylDiv<-join(PhylDiv,pd.result,by="plotcode")
 
+# reorder columns and exclude redundant ones 
 PhylDiv <- PhylDiv[,c(4,1,3,5,7,9,10,2)]
 colnames(PhylDiv) <- c("plotcode", "PSV","PSV_var","PSE","PSR","PSR_var","PD","genus_richness" )
 
 #code NAs as 0 (NAs are produced if the genus richness in a plot is 1, in this case phylogenetic diveristy is 0)
 PhylDiv[,2:7][is.na(PhylDiv[,2:7])] <- 0
+
+# export results 
+write.table(PhylDiv,"PhylDiv_Inv_Genus.txt",sep="\t")
 
